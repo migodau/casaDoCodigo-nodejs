256567d6686511846a893b6a71887871c4db7707
migodau <migodau@gmail.com>
2019-06-29 16:03:56 -0300

    layout

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,75 +1,68 @@
+const LivroDao = require('../infra/livro-dao');
 const db = require('../../config/database');
-const LivroDao = require('../infra/livros-dao')
 
 module.exports = (app) => {
-    app.get('/', function(req, res){
-        res.send(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Casa do Código</h1>
-            </body>
-        </html>`);
-    });
-    
-    app.get('/livros', function(req, res){
-        const livroDao = new LivroDao(db);
-        livroDao.lista().
-        then((livros) => {
-            res.marko(
-                require('../views/livros/lista/lista.marko'),
-                { livros }
-            );
-        })
-        .catch(err => console.log(err));
-    });
-
-    app.get('/livros/form', (req, res) => {
-        res.marko(
-            require('../views/livros/form/form.marko'), { livro: {} }
+    app.get('/', function(req, resp) {
+        resp.marko(
+            require('../views/base/home/home.marko')
         );
     });
+    
+    app.get('/livros', function(req, resp) {
 
-    app.post('/livros', (req, res) => {
         const livroDao = new LivroDao(db);
-        livroDao.adiciona(req.body)
-        .then((livros) => {
-            res.redirect('/livros');
-        })
-        .catch(err => console.log(err));
+        livroDao.lista()
+                .then(livros => resp.marko(
+                    require('../views/livros/lista/lista.marko'),
+                    {
+                        livros: livros
+                    }
+                ))
+                .catch(erro => console.log(erro));
     });
 
-    app.put('/livros', (req, res) => {
-        const livroDao = new LivroDao(db);
-        livroDao.atualiza(req.body)
-        .then((livros) => {
-            res.redirect('/livros');
-        })
-        .catch(err => console.log(err));
-    });
-
-    app.delete('/livros/:id', (req, res) => {
-        const id = req.params.id;
-        const livroDao = new LivroDao(db);
-        livroDao.remove(id)
-        .then(res.status(200).end())
-        .catch(err => console.log(err));
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
     });
 
     app.get('/livros/form/:id', function(req, resp) {
         const id = req.params.id;
         const livroDao = new LivroDao(db);
-    
+
         livroDao.buscaPorId(id)
-            .then(livro => 
-                resp.marko(
-                    require('../views/livros/form/form.marko'),
-                    { livro }
+                .then(livro => 
+                    resp.marko(
+                        require('../views/livros/form/form.marko'), 
+                        { livro: livro }
+                    )
                 )
-            )
-            .catch(erro => console.log(erro));
-    
+                .catch(erro => console.log(erro));
     });
-}
+
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.atualiza(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.delete('/livros/:id', function(req, resp) {
+        const id = req.params.id;
+
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+                .then(() => resp.status(200).end())
+                .catch(erro => console.log(erro));
+    });
+};
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -3,7 +3,6 @@ require('marko/express');
 
 const express = require('express');
 const app = express();
-const rotas = require('../app/rotas/rotas');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
@@ -11,15 +10,17 @@ app.use('/estatico', express.static('src/app/public'));
 
 app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
-      var method = req.body._method
-      delete req.body._method
-      return method
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
     }
-  }))
+}));
+
+const rotas = require('../app/rotas/rotas');
 rotas(app);
 
 module.exports = app;
